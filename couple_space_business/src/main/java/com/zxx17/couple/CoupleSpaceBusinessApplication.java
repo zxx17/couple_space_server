127df7420c79b574c9d0b767dca3d090b15b5ee7
Xinxuan Zhuo <1575165952@qq.com>
2024-10-27 17:36:29 +0800

    init

---
+++ couple_space_business/src/main/java/com/zxx17/couple/CoupleSpaceBusinessApplication.java
@@ -0,0 +1,18 @@
+package com.zxx17.couple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * .
+ *
+ * @author Xinxuan Zhuo
+ * @version 1.0.0
+ * @since 2024/10/23
+ */
+@SpringBootApplication
+public class CoupleSpaceBusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoupleSpaceBusinessApplication.class, args);
+    }
+}
